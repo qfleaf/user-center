80e3e1e442ddd11b0fc705160b7bc961cb1a0770
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-12 11:34:58 +0800

    提供全局异常处理器

---
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.qfleaf.usercenter.common.exception;
+
+import com.qfleaf.usercenter.common.CommonResponse;
+import com.qfleaf.usercenter.common.ResponseCode;
+import com.qfleaf.usercenter.utils.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = BusinessException.class)
+    public CommonResponse<BusinessException> handleBusinessException(BusinessException businessException) {
+        log.info("用户业务异常: {}, 详情: {}", businessException.getMessage(), businessException.getDescription());
+        return ResultUtil.failure(businessException.getResponseCode(), businessException.getDescription());
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public CommonResponse<Void> handleRuntimeException(RuntimeException runtimeException) {
+        log.error("未知异常: {}", runtimeException.getMessage(), runtimeException);
+        return ResultUtil.failure(ResponseCode.ERROR, "服务器繁忙");
+    }
+}
