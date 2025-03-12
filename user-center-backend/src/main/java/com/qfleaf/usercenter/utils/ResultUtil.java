dad59a075dad2eb7eaad3e51d9798ad18a0e36d3
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-12 11:34:14 +0800

    提供构造请求结果的工具类

---
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/utils/ResultUtil.java
@@ -0,0 +1,14 @@
+package com.qfleaf.usercenter.utils;
+
+import com.qfleaf.usercenter.common.CommonResponse;
+import com.qfleaf.usercenter.common.ResponseCode;
+
+public final class ResultUtil {
+    public static <T> CommonResponse<T> success(ResponseCode responseCode, T data) {
+        return new CommonResponse<>(responseCode.getCode(), responseCode.getMsg(), data);
+    }
+
+    public static <T> CommonResponse<T> failure(ResponseCode responseCode, String desc) {
+        return new CommonResponse<>(responseCode.getCode(), desc, null);
+    }
+}
