36877a369a5ac26ef79042dbbf16213fcfa7753d
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-13 11:30:03 +0800

    优化用户列表查询请求参数，提供分页支持

--- user-center-backend/src/main/java/com/qfleaf/usercenter/model/dto/user/UserQueryRequest.java
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/model/dto/user/UserQueryRequest.java
@@ -1,9 +1,12 @@
 package com.qfleaf.usercenter.model.dto.user;
 
+import com.qfleaf.usercenter.model.dto.Pageable;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class UserQueryRequest {
+public class UserQueryRequest extends Pageable {
     private String username;
     private String phone;
     private String email;
