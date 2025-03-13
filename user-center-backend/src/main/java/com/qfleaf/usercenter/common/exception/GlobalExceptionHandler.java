00136f2d559025d8824b7bf08be0552d71da9c5c
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-13 15:43:17 +0800

    提供处理参数异常的方法

--- user-center-backend/src/main/java/com/qfleaf/usercenter/common/exception/GlobalExceptionHandler.java
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/common/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.qfleaf.usercenter.common.CommonResponse;
 import com.qfleaf.usercenter.common.ResponseCode;
 import com.qfleaf.usercenter.utils.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,13 @@ public class GlobalExceptionHandler {
     public CommonResponse<BusinessException> handleBusinessException(BusinessException businessException) {
         log.info("用户业务异常: {}, 详情: {}", businessException.getMessage(), businessException.getDescription());
         return ResultUtil.failure(businessException.getResponseCode(), businessException.getDescription());
+    }
+
+    // 参数校验异常的处理
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonResponse<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        log.debug("参数校验错误: {}", methodArgumentNotValidException.getMessage(), methodArgumentNotValidException);
+        return ResultUtil.failure(ResponseCode.BAD_PARAMS, methodArgumentNotValidException.getMessage());
     }
 
     @ExceptionHandler(value = RuntimeException.class)
