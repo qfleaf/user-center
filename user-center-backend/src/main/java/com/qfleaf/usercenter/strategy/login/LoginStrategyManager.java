4cdd35431621a925776b08b448869ae4159059db
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-17 13:20:36 +0800

    优化登陆策略管理器的策略映射类型为并发包下的实现

--- user-center-backend/src/main/java/com/qfleaf/usercenter/strategy/login/LoginStrategyManager.java
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/strategy/login/LoginStrategyManager.java
@@ -7,15 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component
 public class LoginStrategyManager {
-    private final Map<String, LoginStrategy> loginStrategiesMap = new HashMap<>();
+    private final Map<String, LoginStrategy> loginStrategiesMap = new ConcurrentHashMap<>();
 
     @Autowired
     public LoginStrategyManager(List<LoginStrategy> loginStrategies) {
