5431ef882a11fe1ea2420b9201ae86faea96f4da
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-17 09:20:27 +0800

    提供登陆策略管理器

---
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/strategy/login/LoginStrategyManager.java
@@ -0,0 +1,35 @@
+package com.qfleaf.usercenter.strategy.login;
+
+import com.qfleaf.usercenter.common.ResponseCode;
+import com.qfleaf.usercenter.common.exception.BusinessException;
+import com.qfleaf.usercenter.strategy.login.annotation.LoginType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class LoginStrategyManager {
+    private final Map<String, LoginStrategy> loginStrategiesMap = new HashMap<>();
+
+    @Autowired
+    public LoginStrategyManager(List<LoginStrategy> loginStrategies) {
+        loginStrategies.forEach(loginStrategy -> {
+            LoginType annotation = loginStrategy.getClass().getAnnotation(LoginType.class);
+            if (annotation != null) {
+                loginStrategiesMap.put(annotation.value(), loginStrategy);
+            }
+        });
+        log.debug("loginStrategiesMap: {}", loginStrategiesMap);
+    }
+
+    public LoginStrategy getLoginStrategy(String loginType) {
+        return Optional.ofNullable(loginStrategiesMap.get(loginType))
+                .orElseThrow(() -> new BusinessException(ResponseCode.BAD_PARAMS, "不支持的登陆方式: " + loginType));
+    }
+}
