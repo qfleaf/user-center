3cc76a8f864501f7d6a09c2fdf212254f7f5cb91
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-17 09:19:45 +0800

    提供登陆策略相关常量

---
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/strategy/login/Constants.java
@@ -0,0 +1,8 @@
+package com.qfleaf.usercenter.strategy.login;
+
+public interface Constants {
+    class LoginType {
+        public static final String ACCOUNT = "account";
+        public static final String SMS = "sms";
+    }
+}
