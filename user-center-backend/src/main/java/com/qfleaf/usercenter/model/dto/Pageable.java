e0ec5209914fe01801ce13eb96f29185b35a1dca
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-17 11:46:53 +0800

    修复校验bug

--- user-center-backend/src/main/java/com/qfleaf/usercenter/model/dto/Pageable.java
+++ user-center-backend/src/main/java/com/qfleaf/usercenter/model/dto/Pageable.java
@@ -1,15 +1,15 @@
 package com.qfleaf.usercenter.model.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public abstract class Pageable {
     @NotNull(message = "页码不能为空")
-    @Size(min = 1, message = "页码不能小于1")
+    @Min(value = 1, message = "页码不能小于1")
     protected Integer current;
     @NotNull(message = "页大小不能为空")
-    @Size(min = 5, message = "页大小不能小于5")
+    @Min(value = 5, message = "页大小不能小于5")
     protected Integer size;
 }
